40bd1789c42fa7150f932afa6239bc88b70b577d
Lucas <lucasbolelasilveira@gmail.com>
2023-04-06 12:07:51 -0300

    fix: pdf reader

--- rasp/viewspdf.py
+++ rasp/viewspdf.py
@@ -1,25 +1,20 @@
-import tika
-import fitz
-import psycopg2
+import io
 from django.http import HttpResponse
+import psycopg2 as pg
 from django.conf import settings
+import requests
+from PyPDF2 import PdfReader
 
 def Inportpdf(request, caminho, id, tabela, campo, indice):
-    html = "<html><body>Done! %s </body></html>"
-
-    import tika
-    import psycopg2
-    from tika import parser
-    from django.http import HttpResponse
-    raw = parser.from_file(caminho)
-    dados = raw['content']
-    print(dados)
-    # conn = psycopg2.connect(
-    #     host="localhost",
-    #     database="DPP",
-    #     user="postgres",
-    #     password="3621"
-    # )
+    html = "<html><body>Done!</body></html>"
+    r = requests.get(caminho, stream=True)
+    f = io.BytesIO(r.content)
+    
+    reader = PdfReader(f)
+    texto = ''
+    for pagina in reader.pages:
+        texto += pagina.extract_text()
+    
     conn = pg.connect(
         user=settings.DB_USER,
         password=settings.DB_PASSWORD,
@@ -27,10 +22,11 @@ def Inportpdf(request, caminho, id, tabela, campo, indice):
         port=settings.DB_PORT,
         database=settings.DB_NAME
     )
+    
     curs = conn.cursor()
-    query = (f"""Update {tabela} set {campo} = ('{dados}') where {indice} = {id}""")
+    query = (f"""Update {tabela} set {campo} = ('{texto}') where {indice} = {id}""")
+    
     print(query)
     curs.execute(query)
     conn.commit()
-
     return HttpResponse(html)
